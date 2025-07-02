dd4dfc588c33322695375a1c9a01b586f923096f
bunnyooi <imtalhabinsohaill@gmail.com>
2025-07-02 15:34:21 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div className="text-purple-600 text-2xl">✅ It works! Your YouTube Filter Frontend is running.</div>;
+}
+
+export default App;
